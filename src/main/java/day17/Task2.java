eaff5a321920d12ad85a912098246793723ac728
Mark Kraskovskii <kraskovskii.m@yandex.ru>
2020-11-15 19:26:12 +0300

    complete day18 tasks

--- src/main/java/day17/Task2.java
+++ src/main/java/day17/Task2.java
@@ -8,7 +8,7 @@ public class Task2 {
                 {ChessPiece.PAWN_BLACK,     ChessPiece.EMPTY,       ChessPiece.KNIGHT_BLACK,    ChessPiece.EMPTY,           ChessPiece.EMPTY,           ChessPiece.EMPTY,       ChessPiece.PAWN_BLACK,  ChessPiece.EMPTY},
                 {ChessPiece.QUEEN_BLACK,    ChessPiece.EMPTY,       ChessPiece.EMPTY,           ChessPiece.BISHOP_WHITE,    ChessPiece.EMPTY,           ChessPiece.EMPTY,       ChessPiece.EMPTY,       ChessPiece.EMPTY},
                 {ChessPiece.EMPTY,          ChessPiece.EMPTY,       ChessPiece.EMPTY,           ChessPiece.BISHOP_BLACK,    ChessPiece.PAWN_WHITE,      ChessPiece.EMPTY,       ChessPiece.EMPTY,       ChessPiece.EMPTY},
-                {ChessPiece.EMPTY,          ChessPiece.EMPTY,       ChessPiece.EMPTY,           ChessPiece.EMPTY,           ChessPiece.BISHOP_WHITE,    ChessPiece.PAWN_WHITE,  ChessPiece.EMPTY,       ChessPiece.EMPTY, ChessPiece.EMPTY,},
+                {ChessPiece.EMPTY,          ChessPiece.EMPTY,       ChessPiece.EMPTY,           ChessPiece.EMPTY,           ChessPiece.BISHOP_WHITE,    ChessPiece.PAWN_WHITE,  ChessPiece.EMPTY,       ChessPiece.EMPTY},
                 {ChessPiece.PAWN_WHITE,     ChessPiece.EMPTY,       ChessPiece.EMPTY,           ChessPiece.QUEEN_WHITE,     ChessPiece.EMPTY,           ChessPiece.PAWN_WHITE,  ChessPiece.EMPTY,       ChessPiece.PAWN_WHITE},
                 {ChessPiece.EMPTY,          ChessPiece.EMPTY,       ChessPiece.EMPTY,           ChessPiece.EMPTY,           ChessPiece.EMPTY,           ChessPiece.ROOK_WHITE,  ChessPiece.KING_WHITE,  ChessPiece.EMPTY}
         });
--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -1,7 +1,32 @@
+//        1. Вам дан числовой массив произвольного размера. Посчитайте сумму чисел в этом массиве
+//        не используя циклы (необходимо использовать рекурсивные вызовы).
+//
+//        Пример:
+//        int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+//        System.out.println(recursionSum(...)); // 65346
+//
+//        *recursionSum(...) - это ваш рекурсивный метод, который вернет сумму чисел в массиве numbers.
+
 package day18;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.util.Random;
 
+public class Task1 {
+        public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = new int[random.nextInt(100)];
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100500);
+        }
+        System.out.println(recursionSum(array, 0));
+    }
+
+    public static int recursionSum(int[] array, int i) {
+        if (i == array.length) {
+            return 0;
+        }
+        return array[i] + recursionSum(array, i+1);
     }
 }
+
